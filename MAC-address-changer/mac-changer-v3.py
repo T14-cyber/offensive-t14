a8aba0fed80b5a1696bf41b0381bb887f999d967
t14-cyber <t14-cyber@tutanota.com>
2023-12-23 20:55:58 +0100

    added safety feature

---
+++ MAC-address-changer/mac-changer-v3.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python
+import subprocess
+
+#creating vars
+#getting input from the user
+interface = input("Enter the interface name: ")
+new_mac_address = input("Enter the new MAC address: ")
+
+#adding a print statement for the visual comm
+
+print("Changing the MAC address of interface " + interface + "to " + new_mac_address)
+#protecting from vulnerabilities. dafely handle the user input
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
+subprocess.call(["ifconfig",interface,"up"])
+
