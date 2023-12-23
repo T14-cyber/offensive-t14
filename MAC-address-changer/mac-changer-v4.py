9408b7ad6982a152ab14d1e413990f4217371247
t14-cyber <t14-cyber@tutanota.com>
2023-12-23 22:04:03 +0100

    version 4

--- MAC-address-changer/mac-changer-v4.py
+++ MAC-address-changer/mac-changer-v4.py
@@ -5,12 +5,12 @@ import optparse
 reader = optparse.OptionParser()
 
 reader.add_option("-i", "--interface", dest="interface", help="interface name")
-
+reader.add_option("-m", "--mac", dest="new_mac_address", help="New Mac Address")
+(values, keys)= reader.parse_args()
 #creating vars
 #getting input from the user
-interface = input("Enter the interface name: ")
-new_mac_address = input("Enter the new MAC address: ")
-
+interface = values.interface
+new_mac_address =  values.new_mac_address
 #adding a print statement for the visual comm
 
 print("Changing the MAC address of interface " + interface + "to " + new_mac_address)
